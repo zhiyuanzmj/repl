f6b026838d69490c92236625143ab8219dd2ee21
zhiyuanzmj <260480378@qq.com>
2025-02-21 19:37:34 +0800

    feat(prisma): extends @prisma/extension-accelerate

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 export const prisma = new PrismaClient({
   errorFormat: 'minimal',
-})
+}).$extends(withAccelerate())
