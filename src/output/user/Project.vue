5f0056de7a5608cde1bdd53fb28b8a6bd120a3c3
zhiyuanzmj <260480378@qq.com>
2025-02-25 15:21:59 +0800

    test3

--- src/output/user/Project.vue
+++ src/output/user/Project.vue
@@ -4,7 +4,7 @@ import { computed, inject, ref, watch } from 'vue'
 import { ofetch } from 'ofetch'
 import { type Project, injectKeyProps } from '../../types'
 
-const test = 2
+const test = 3
 
 const { store } = inject(injectKeyProps)!
 
