66bcb5f9c4182a495b9fdbf3ceda1ef7648e717f
zhiyuanzmj <260480378@qq.com>
2025-05-15 10:25:32 +0800

    fix: use login instead of name for user-info

--- server/routes/api/oauth/redirect.ts
+++ server/routes/api/oauth/redirect.ts
@@ -35,7 +35,7 @@ export default defineEventHandler(async (event) => {
     user = await prisma.user.create({
       data: {
         githubId: data.id,
-        name: data.name || data.login,
+        name: data.login || data.name,
         email: data.email,
         avatar: data.avatar_url,
       },
